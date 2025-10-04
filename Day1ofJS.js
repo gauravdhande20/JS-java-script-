2db76c37727c86ecc022bdcd6b95fa845fa99abe
gauravdhande20 <gauravdhande2022004@gmail.com>
2025-10-04 17:51:55 +0530

    day one

---
+++ Day1ofJS.js
@@ -0,0 +1,29 @@
+console.log("this is my first code of js")
+
+// Variables in JS
+
+let a=6
+let b="this is string"
+let c=true
+let d=123.4
+let e=null
+let f=undefined
+
+// to know the data type use (typeof)
+console.log(typeof 6)
+console.log(typeof "this is string")
+console.log(typeof true)
+console.log(typeof 123.4)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+// Outout:
+
+// this is my first code of js
+//   number
+// string
+// boolean
+// number
+// object
+// undefined
