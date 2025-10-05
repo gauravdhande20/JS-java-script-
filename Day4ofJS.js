e63332cffa9acfdeecd28c36fb85f47c10e62f19
gauravdhande20 <gauravdhande2022004@gmail.com>
2025-10-05 23:51:53 +0530

    day 4

---
+++ Day4ofJS.js
@@ -0,0 +1,37 @@
+// Function in  JS
+
+// Funcation is a building block  in all programming
+
+
+console.log("let's create first function ")
+
+function youeName(name){
+    console.log("hey " +name+ " how are you")
+    console.log("hey " +name+ " can i help you")
+    
+}
+youeName("Gaurav")
+
+
+
+function sum(a,b){
+    // console.log(a+b)
+    return a+b;
+}
+result1=sum(8,9)
+console.log("the sum of a and b is: ",result1)
+result2=sum(3,9)
+console.log("the sum of a and b is: ",result2)
+result3=sum(9,9)
+console.log("the sum of a and b is: ",result3)
+
+
+// -----Funcation using  variable----------
+// Arrow function
+
+const fun1=(x)=>{
+    console.log("the number is : ",x)
+}
+fun1(34)
+fun1(314)
+fun1(14)
