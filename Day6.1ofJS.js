01e9e9be4fd9308a1ce5cad05ee4bdc54a5509de
gauravdhande20 <gauravdhande2022004@gmail.com>
2025-10-06 14:18:04 +0530

    Day6.1ofJS

---
+++ Day6.1ofJS.js
@@ -0,0 +1,35 @@
+// Array in JS
+
+
+let arr=[2,6,1,4,9,8]
+console.log(arr)
+
+// to print the length of array
+console.log("the length of array is :"+arr.length)  
+
+
+// to access the element from it's index number
+console.log("at index no. 2 the number is:",arr[2])
+console.log("at index no. 5 the number is:",arr[5])
+console.log("at index no. 3 the number is:",arr[3])
+
+// to chage value to array from index number
+console.log(arr)
+
+arr[1]=234
+arr[4]=100
+console.log(arr)
+
+let str=["gaurav","vansh","vaibhav"]
+console.log(str)
+
+
+// output:
+// [ 2, 6, 1, 4, 9, 8 ]
+// the length of array is :6
+// at index no. 2 the number is: 1
+// at index no. 5 the number is: 8
+// at index no. 3 the number is: 4
+// [ 2, 6, 1, 4, 9, 8 ]
+// [ 2, 234, 1, 4, 100, 8 ]
+// [ 'gaurav', 'vansh', 'vaibhav' ]
