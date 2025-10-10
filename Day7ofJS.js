e3a0664a1411615b5c506470074fab87bdb6521e
gauravdhande20 <gauravdhande2022004@gamil.com>
2025-10-11 01:11:12 +0530

    Day7ofJS

---
+++ Day7ofJS.js
@@ -0,0 +1,24 @@
+// loops in JS
+
+let a=0
+while(a<5){
+    console.log("to print number ",a)
+    a++
+}
+
+
+// do while loop
+
+console.log("do while loop")
+let b=5
+do{
+    console.log(b)
+    a++
+}while(b>=15)  //it will print atleast one time
+
+// for loop
+    let c=2
+
+    for(let i=0;i<=10;i++){
+        console.log(c+" * " +i+"= ",c*i)
+    }  
