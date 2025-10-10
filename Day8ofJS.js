af218b95073c0f678f6bd246d8ac3185e584f71b
gauravdhande20 <gauravdhande2022004@gamil.com>
2025-10-11 01:25:18 +0530

    Day8ofJS

---
+++ Day8ofJS.js
@@ -0,0 +1,15 @@
+// built in funcation in JS
+
+console.log("built in funcation in JS")
+
+// alert
+
+alert("this is alert")
+
+// prompt
+let age=prompt("enter your age")
+
+console.log("your age is ",age)
+
+// confirm
+let confirmAge=confirm("are you sure you want to delete this page")
