21bf137bc3c06565eacba2989908928cc0a6a6c7
gauravdhande20 <gauravdhande2022004@gamil.com>
2025-10-16 20:56:05 +0530

    Day9.3ofJS

---
+++ Day9.3ofJS.js
@@ -0,0 +1,35 @@
+// number Properties in javaScript
+console.log("number Properties in javaScript")
+// output: number Properties in javaScript
+
+// MAX_VALUE property
+console.log(Number.MAX_VALUE); // it will return the maximum value of a number
+// output: 1.7976931348623157e+308  
+
+// MIN_VALUE property   
+console.log(Number.MIN_VALUE); // it will return the minimum value of a number
+// output: 5e-324
+
+// POSITIVE_INFINITY property
+console.log(Number.POSITIVE_INFINITY); // it will return the positive infinity value
+// output: Infinity
+
+// NEGATIVE_INFINITY property
+console.log(Number.NEGATIVE_INFINITY); // it will return the negative infinity value
+// output: -Infinity
+
+// NaN property
+console.log(Number.NaN); // it will return the NaN value
+// output: NaN
+
+// EPSILON property
+console.log(Number.EPSILON); // it will return the smallest difference between two representable numbers
+// output: 2.220446049250313e-16
+
+// Safe integer properties
+console.log(Number.MAX_SAFE_INTEGER); // it will return the maximum safe integer value
+// output: 9007199254740991
+
+console.log(Number.MIN_SAFE_INTEGER); // it will return the minimum safe integer value
+// output: -9007199254740991
+
