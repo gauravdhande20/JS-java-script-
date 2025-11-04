1c63e3ad4229c7ebf00ddec7fc790fc504c8f645
gauravdhande20 <gauravdhande2022004@gamil.com>
2025-11-04 14:27:33 +0530

    day10ofJs

---
+++ Day10ofJS.js
@@ -0,0 +1,19 @@
+// Objects i JavaScript
+
+console.log("Objects in JS")
+
+let person={
+    name: "John",
+    age: 30,
+    city: "New York"
+}
+
+document.writeinfo=`
+Name: ${person.name} <br>
+Age: ${person.age} <br>
+City: ${person.city} <br>
+`
+document.writeln(document.writeinfo)
+
+
+
